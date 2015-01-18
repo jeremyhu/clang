be249dfc99009e07f65c01463ac62d9592aa00d7
Jeremy Huddleston Sequoia <jeremyhu@apple.com>
2015-01-17 17:50:24 -0800

    MacPorts Only: Prepare clang-format for replacement with absolute path
    Signed-off-by: Jeremy Huddleston Sequoia <jeremyhu@apple.com>
    (cherry picked from commit d95bdf342037ee815fa21e871dd9479bc3572550)
    
    Conflicts:
    	tools/clang-format/clang-format.el
    	tools/clang-format/clang-format.py

--- tools/clang-format/clang-format.el
+++ tools/clang-format/clang-format.el
@@ -13,7 +13,7 @@
 
 ;; *Location of the clang-format binary. If it is on your PATH, a full path name
 ;; need not be specified.
-(defvar clang-format-binary "clang-format")
+(defvar clang-format-binary "@CLANG_FORMAT_PATH@")
 
 (defun clang-format-region ()
   "Use clang-format to format the currently active region."
